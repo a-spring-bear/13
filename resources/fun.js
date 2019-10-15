38e3c616b85a7c69baee31cbef7700789ca6a479
a-spring-bear <768022147@qq.com>
2019-10-15 12:43:54 +0800

    历史战局详情还未完成

--- resources/fun.js
+++ resources/fun.js
@@ -56,6 +56,7 @@ function validate()
 }
 //注销
 function logout() {
+    token =localStorage.getItem("token1");
     $.ajax({
         type: "post",
         url: "https://api.shisanshui.rtxux.xyz/auth/logout",
@@ -129,6 +130,7 @@ function opennnn() {
 function play(){
 
     user_id=localStorage.getItem("id");
+    token =localStorage.getItem("token1");
     console.log(user_id);
     console.log(JSON.stringify({
         "card":[c1,c2,c3]
@@ -280,170 +282,65 @@ function show() {
 }
 //排行榜在HTML文件中
 
-//历史记录列表
+// //历史战局详情
+var x=0;//页码
+function add(){document.getElementById("demo").innerHTML=null;x++;myhis(); }
+function dec(){if(x>0){document.getElementById("demo").innerHTML=null;x--;myhis();} else {alert("前面没有页数啦！"); return;}}
 function myhis() {
-    //获取输入框的值
-        var player_id=document.getElementById("player_id").value;
-        var limit= document.getElementById("pageSize").value;
-        var page=document.getElementById("changePage").value;
 
-//     var a = [
-//         {
-//             "id": 2890,
-//             "card": [
-//                 "&J $K &A",
-//                 "#2 &2 #3 *3 $5",
-//                 "&3 #4 *5 $6 #7"
-//             ],
-//             "score": -7,
-//             "timestamp": 1570608064
-//         },
-//         {
-//             "id": 2897,
-//             "card": [
-//                 "$9 $Q *A",
-//                 "#3 $3 *3 $K &K",
-//                 "*5 *10 #10 $10 &10"
-//             ],
-//             "score": 24,
-//             "timestamp": 1570608065
-//         },
-//         {
-//             "id": 2900,
-//             "card": [
-//                 "&3 *8 *J",
-//                 "&5 *6 &7 &8 #9",
-//                 "$2 $4 $6 $8 $Q"
-//             ],
-//             "score": -2,
-//             "timestamp": 1570608066
-//         },
-//         {
-//             "id": 2894,
-//             "card": [
-//                 "#5 #J *A",
-//                 "*3 &4 *10 $K &K",
-//                 "$3 $6 $8 $9 $A"
-//             ],
-//             "score": -4,
-//             "timestamp": 1570608065
-//         },
-//         {
-//             "id": 2896,
-//             "card": [
-//                 "#2 &10 $K",
-//                 "&3 *6 $6 $7 &7",
-// //                 "#9 &9 $9 #A *A"
-// //             ],
-// //             "score": 1,
-// //             "timestamp": 1570608065
-// //         },
-// //         {
-// //             "id": 2899,
-// //             "card": [
-// //                 "*8 &9 #K",
-// //                 "*2 &5 *5 *6 #6",
-// //                 "$7 $8 #9 $10 $J"
-// //             ],
-// //             "score": -8,
-// //             "timestamp": 1570608066
-//         },{
-//             "id": 2898,
-//             "card": [
-//                 "&6 *7 #K",
-//                 "*10 $J #J &A *A",
-//                 "#2 $4 *8 &8 $8"
-//             ],
-//             "score": -13,
-//             "timestamp": 1570608065
-//         },
-//         {
-//             "id": 2909,
-//             "card": [
-//                 "*4 *10 #Q",
-//                 "&2 *3 $3 &J $J",
-//                 "$4 $5 #6 &7 &8"
-//             ],
-//             "score": -11,
-//             "timestamp": 1570608068
-//         },
-//         {
-//             "id": 2912,
-//             "card": [
-//                 "&2 &8 &A",
-//                 "#3 #4 #5 #10 #A",
-//                 "$6 #6 *6 $9 &9"
-//             ],"score": 6,
-//             "timestamp": 1570608069
-//         },
-//         {
-//             "id": 2968,
-//             "card": [
-//                 "$7 &10 &J",
-//                 "&3 #4 $4 $A *A",
-//                 "&5 #9 #Q *Q $Q"
-//             ],
-//             "score": -6,
-//             "timestamp": 1570608081
-//         }
-//
-//     ]
-// add(a);
+    var json;
+    var token = localStorage.getItem('token1');
+    (function ($) {
+        $.fn.serializeJson = function () {
+            var serializeObj = {};
+            var array = this.serializeArray();
+            var str = this.serialize();
+            $(array).each(function () {
+                if (serializeObj[this.name]) {
+                    if ($.isArray(serializeObj[this.name]) && this.value != "") {
+                        serializeObj[this.name].push(this.value);
+                    } else {
+                        if (this.value != "") {
+                            serializeObj[this.name] = [serializeObj[this.name], this.value];
+                        }
+                    }
+                } else {
+                    if (this.value != "") {
+                        serializeObj[this.name] = this.value;
+                    }
+                }
+            });
+            return serializeObj;
+        };
+    })(jQuery);
+$.ajax({
+    type: "GET",
+    url: "https://api.shisanshui.rtxux.xyz/history?page="+x+"&limit=6&player_id=1",
+    headers: {
+        'x-auth-token': token
+    } ,
+    success: function (result) {
+        console.log(result);
+        console.log(token);
+        json = result;
+        function getJsonLength(json) {
+            for (var i in json) {
+                var l = 0;
 
-    token = localStorage.getItem("token1");
-    $.ajax({
-        url: "https://api.shisanshui.rtxux.xyz/history",
-        type: "GET",
-        headers:{"X-Auth-Token":token},
-        data: JSON.stringify({
-            "player_id": player_id,
-            "limit": limit,
-            "page": page
-        }),
+                for (var j in json.data) {
+                    document.getElementById("demo").innerHTML += '<tr><td style="width: 10% ;height: 20px" >' + json.data[l].id + '</td><td style="width: 65%;text-align: center">' + json.data[l].card + '</td><td style="width: 10%">' + json.data[l].score + '</td><td style="width: 15%">' + json.data[l].timestamp + '</td></tr>';
+                    l++;
+                };
 
-        contentType: "application/json",
-        datatype: "json",
-        success: function (res) {
-            console.log(res);
-            console.log("success");
-            add(res.data);
-        },
-    });
+            };
 
-}
-//<!--    动态加入数据-->
-function add(per) {
-
-    var tbody = document.getElementById('tbMain');
-
-    for(var i = 0;i < per.length; i++){ //遍历一下json数据  
-        var trow = getDataRow(per[i]); //定义一个方法,返回tr数据  
-        tbody.appendChild(trow);
+        }
+        document.getElementById("p").innerHTML=x+1;
+        getJsonLength(json);
     }
+});
+
 }
-//添加一行数据
-function getDataRow(h){
-    var row = document.createElement('tr'); //创建行  
-
-    var idCell = document.createElement('td'); //创建第一列id  
-    idCell.innerHTML = h.id; //填充数据  
-    row.appendChild(idCell); //加入行  ，下面类似  
-
-    var timeCell = document.createElement('td');//创建第二列name  
-    timeCell.innerHTML = h.timestamp;
-    row.appendChild(timeCell);
-
-    var carCell = document.createElement('td');//创建第三列job  
-    carCell.innerHTML = h.card;
-    row.appendChild(carCell);
-
-    var scoreCell = document.createElement('td');//创建第三列job  
-    scoreCell.innerHTML = h.score;
-    row.appendChild(scoreCell);
-
-    return row; //返回tr数据      
-}
-//历史战局详情
 function his() {
     var id=document.getElementById("id").value;
     token =localStorage.getItem("token1");
@@ -475,10 +372,10 @@ function his() {
             // document.getElementById("33").innerHTML=test.data.detail[2].card;
             // document.getElementById("43").innerHTML=test.data.detail[3].card;
 
-           var s1=test.data.detail[0].card.trim.split(/\s+/);
-            var s2=test.data.detail[0].card.trim.split(/\s+/);
-            var s3=test.data.detail[0].card.trim.split(/\s+/);
-            var s4=test.data.detail[0].card.trim.split(/\s+/);
+           var s=test.data.detail[0].card.trim.split(/\s+/);
+            s=s+test.data.detail[1].card.trim.split(/\s+/);
+            s=s+test.data.detail[2].card.trim.split(/\s+/);
+            s=s+test.data.detail[3].card.trim.split(/\s+/);
 
             document.getElementById("14").innerHTML=test.data.detail[0].score;
             document.getElementById("24").innerHTML=test.data.detail[1].score;
